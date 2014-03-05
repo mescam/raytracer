c0bd4b1dc346a74268072521e273b621334f3ad4
mug3tsu <mug3tsu92@gmail.com>
2014-03-05 22:22:22 +0100

    bug fixes for Raytracer class

--- include/Raytracer.h
+++ include/Raytracer.h
@@ -2,28 +2,14 @@
 #define _RAYTRACER_H_
 
 #include "Image.h"
+#include "Scene.h"
+#include "Ray.h"
+#include "Camera.h"
 
 class Raytracer {
 public:
     Raytracer() {}
-    Image raytrace(Scene scene, Camera camera, int w, int h) {
-        Image img(w,h);
-        for(int x = 0; x < w, x++) {
-            for (int y = 0; y < h; y++) {
-                Vector2 pictureCoordinates = Vector2(((x + 0.5) / (double)w) * 2 - 1),((y + 0.5) / (double)h) * 2 - 1);
-                Ray ray = camera.getRayTo(pictureCoordinates);
-                HitInfo info = scene.traceRay(ray);
-                Color color;
-                if (info.HitObject) {
-                    color = info.color;
-                } else {
-                    color = scene.backgroundColor;
-                }
-                img.setPixel(x,y,color);
-            }
-        }
-        return img;
-    }
+    Image raytrace(Scene scene, Camera* camera, int w, int h);
 };
 
 #endif //_RAYTRACER_H_
--- src/Raytracer.cpp
+++ src/Raytracer.cpp
@@ -1,1 +1,20 @@
 #include "Raytracer.h"
+
+Image Raytracer::raytrace(Scene scene, Camera* camera, int w, int h) {
+    Image img(w,h);
+    for(int x = 0; x < w; x++) {
+        for (int y = 0; y < h; y++) {
+            Vector2 pictureCoordinates = Vector2((((x + 0.5)/(double)w) * 2 - 1),(((y + 0.5)/(double)h)* 2 - 1));
+            Ray ray = camera->getRayTo(pictureCoordinates);
+            HitInfo info = scene.traceRay(ray);
+            Color color;
+            if (info.hitObject) {
+                color = info.color;
+            } else {
+                color = scene.getBackgroundColor();
+            }
+            img.setPixel(x,y,color);
+        }
+    }
+    return img;
+}

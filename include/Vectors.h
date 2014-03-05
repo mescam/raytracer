f56bd83f8e4959c175fafcd0f4729f9b2c4813e2
mug3tsu <mug3tsu92@gmail.com>
2014-03-05 13:10:17 +0100

    Added basic Vector2 class, corrected some errors

--- include/Vectors.h
+++ include/Vectors.h
@@ -21,6 +21,7 @@ public:
     void setY(double y);
     void setZ(double z);
 
+    Vector3 operator!();
     Vector3 operator+(const Vector3 & v);
     Vector3 operator-(const Vector3 & v);
     Vector3 operator*(const double & val);
@@ -30,9 +31,25 @@ public:
     static Vector3 cross(Vector3 a, Vector3 b);
 
     double length();
-    double length2();
+    double lengthSq();
 
     Vector3 getNormalized();
+};
+
+class Vector2 {
+public:
+    double x;
+    double y;
+public:
+    Vector2(double x, double y): x(x), y(y) {
+
+    }
+
+    double getX();
+    double getY();
+
+    void setX(double x);
+    void setY(double y);
 };
 
 #endif //_VECTORS_H_
--- src/Vectors.cpp
+++ src/Vectors.cpp
@@ -1,5 +1,6 @@
 #include "Vectors.h"
 
+// == Vector3 ==
 
 double Vector3::getX() {
     return this->x;
@@ -21,39 +22,55 @@ void Vector3::setZ(double z) {
     this->z = z;
 }
 
-
+Vector3 Vector3::operator!() {
+    return Vector3(-(this->x),-(this->y),-(this->z));
+}
 Vector3 Vector3::operator+(const Vector3 & v) {
     return Vector3(this->x+v.x, this->y+v.y, this->z+v.z);
 }
 Vector3 Vector3::operator-(const Vector3 & v) {
     return Vector3(this->x-v.x, this->y-v.y, this->z-v.z);
-
 }
 Vector3 Vector3::operator*(const double & val) {
     return Vector3(this->x*val, this->y*val, this->z*val);
-
 }
 Vector3 Vector3::operator/(const double & val) {
 	return Vector3(this->x/val, this->y/val, this->z/val);
 }
 
 
-static double Vector3::dot(Vector3 a, Vector3 b) {
+double Vector3::dot(Vector3 a, Vector3 b) {
     return (a.x*b.x + a.y*b.y + a.z*b.z);
 }
 
-static Vector3 Vector3::cross(Vector3 a, Vector3 b) {
+Vector3 Vector3::cross(Vector3 a, Vector3 b) {
     return Vector3(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x);
 }
 
 double Vector3::length() {
-    return sqrt(this->length2());
+    return sqrt(this->lengthSq());
 }
     
-double Vector3::length2() {
+double Vector3::lengthSq() {
     return (this->x*this->x + this->y*this->y + this->z*this->z);
 }
 
 Vector3 Vector3::getNormalized() {
     return ((*this)/this->length());
+}
+
+// == Vector2 ==
+
+double Vector2::getX() {
+    return this->x;
+}
+double Vector2::getY() {
+    return this->y;
+}
+
+void Vector2::setX(double x) {
+    this->x = x;
+}
+void Vector2::setY(double y) {
+    this->y = y;
 }
